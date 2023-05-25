8475f180cefc46992e670f764485a45a50d21f6e
rnogowski <66251490+rnogowski@users.noreply.github.com>
2023-05-25 09:22:19 +0000

    1st change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 function App() {
+  const name = 'StarGazers';
   return (
+    <div className="container">
+    <article>
     <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+      <img src="images/group.svg" alt="StarGazers Group" />
+      <h1>Meet the {name}</h1>
+      <p>Members of an <b>intergalactic alliance</b> <br/>
+      paving the way for peace and benevolence among all species. <br/>They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
     </hgroup>
+    </article>
+    </div>
   )
 }
 export default App
